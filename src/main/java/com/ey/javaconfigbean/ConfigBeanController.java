871dd96e73c54734049dfb9d231f8ee4357694f5
SM871YP <Thiyagaraja.Saravanakumar@gds.ey.com>
2023-07-11 17:10:19 +0530

    azure-db-connect version2

--- src/main/java/com/ey/javaconfigbean/ConfigBeanController.java
+++ src/main/java/com/ey/javaconfigbean/ConfigBeanController.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 public class ConfigBeanController {
     private Coach myCoach;
 
